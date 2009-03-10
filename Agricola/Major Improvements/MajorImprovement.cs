1b5bb80b14312b3f7c165b40f790000dee7d0b1e
Tyler Butler <tyler@tylerbutler.com>
2009-03-10 00:22:24 -0700

    Added Major Improvement class stubs.

---
+++ Agricola/Major Improvements/MajorImprovement.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace agricola
+{
+    public abstract class MajorImprovement : IImprovement
+    {
+        #region IImprovement Members
+
+        public int ClayCost
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int StoneCost
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int WoodCost
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int ReedCost
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+    }
+}
--- Agricola/Major Improvements/Well.cs
+++ Agricola/Major Improvements/Well.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace agricola
 {
-    public class MajorImprovement : Improvement
+    public class Well : MajorImprovement
     {
     }
 }
